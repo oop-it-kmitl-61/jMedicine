618450b89d10532d652c0ad5fa6514c1779ae33f
Wiput Pootong <wiput.pootong@gmail.com>
2018-11-13 13:16:12 +0700

    :art: improve Gradlefile

--- src/main/Database.java
+++ src/main/Database.java
@@ -1,7 +1,26 @@
 package main;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class Database {
 
+  public static void main(String[] args) throws SQLException {
+    String url = "jdbc:postgresql://jmed.wiput.me/jmedicine";
+    Properties props = new Properties();
+    props.setProperty("user", "jmedicine");
+    props.setProperty("password", "Jmedicine#2018");
+    props.setProperty("ssl", "false");
+    Connection conn = DriverManager.getConnection(url, props);
+
+    Statement st = conn.createStatement();
+    ResultSet rs = st.executeQuery("SELECT * FROM test");
+
+    while (rs.next()) {
+      System.out.println(rs.getString(1));
+    }
+    
+    rs.close();
+    st.close();
+  }
 }
