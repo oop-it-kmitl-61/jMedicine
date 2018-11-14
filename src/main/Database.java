fd84809983557a45139a27d475d296a2fb17165d
Wiput Pootong <wiput.pootong@gmail.com>
2018-11-14 17:01:03 +0700

    :sparkles: add database connection method

--- src/main/Database.java
+++ src/main/Database.java
@@ -1,7 +1,20 @@
 package main;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class Database {
 
+  public static Connection getConnection() throws SQLException {
+    String url = "jdbc:postgresql://jmed.wiput.me/jmedicine";
+    Properties props = new Properties();
+    props.setProperty("user", "jmedicine");
+    props.setProperty("password", "Jmedicine#2018");
+    props.setProperty("ssl", "false");
+    return DriverManager.getConnection(url, props);
+  }
+
+  public static void main(String[] args) throws SQLException {
+    getConnection();
+  }
 }
