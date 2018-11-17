559ef48fc1667645335132bb14b45d030cb9b06c
Wiput Pootong <wiput.pootong@gmail.com>
2018-11-17 19:32:23 +0700

    :sparkles: add login class and sha256

--- src/api/Login.java
+++ src/api/Login.java
@@ -1,12 +1,27 @@
 package api;
 
+import static core.Utils.sha256;
+
 import core.Database;
+import core.User;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Login {
-  private Connection connection = Database.getConnection();
+
+  private Connection connection;
 
   public Login() throws SQLException {
+    Connection connection = Database.getConnection();
   }
+
+  public User doSignIn(String username, String password) throws NoSuchAlgorithmException {
+    password = sha256(password);
+
+    return new User("Hello", "male", "112", "120", "120");
+  }
+
+
+
 }
