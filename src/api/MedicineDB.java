7f385c1aa50809612a54eecb702eec360734c2b1
Wiput Pootong <wiput.pootong@gmail.com>
2018-11-19 15:26:14 +0700

    :sparkles: add medicine database

---
+++ src/api/MedicineDB.java
@@ -0,0 +1,57 @@
+package api;
+
+
+import core.Database;
+import core.Medicine;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MedicineDB {
+
+  private static Connection connection;
+
+  static {
+    try {
+      connection = Database.getConnection();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static ArrayList<Medicine> getAllMedicine(String userId)
+      throws SQLException {
+
+    String SQLCommand = "SELECT * FROM medicine WHERE \"user\" = ?";
+
+    PreparedStatement pStatement = connection.prepareStatement(SQLCommand);
+    pStatement.setObject(1, userId, Types.OTHER);
+
+    ResultSet result = pStatement.executeQuery();
+
+    ArrayList<Medicine> results = new ArrayList<>();
+
+    while (result.next()) {
+      ArrayList<String> time = Arrays.stream((Object[]) result.getArray("time").getArray())
+          .map(Object::toString).collect(Collectors.toCollection(ArrayList::new));
+
+      ArrayList<String> doseStr = Arrays.stream((Object[]) result.getArray("doseStr").getArray())
+          .map(Object::toString).collect(Collectors.toCollection(ArrayList::new));
+
+      results.add(new Medicine(result.getString("name"), result.getString("type"),
+          result.getString("color"), result.getString("description"), time,
+          doseStr, result.getInt("dose"), result.getInt("total"),
+          result.getString("expire")));
+    }
+
+    pStatement.close();
+    connection.close();
+
+    return results;
+  }
+}
